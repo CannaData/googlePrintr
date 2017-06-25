268289069805b048d4b3e425bbe3dc66b7781933
carlganz <carlganz@gmail.com>
2017-06-25 10:51:01 -0700

    basic working example

--- R/gcp.R
+++ R/gcp.R
@@ -1,7 +1,12 @@
 #' List printers associated with Google account
 #'
-#' @param name String for searching printer names
+#' @param name Query printer names
+#' @param type Query printer type, see notes
+#' @param connection_status Query connection status, see notes
+#' @param use_cdd Indicate whether to include printer info
+#' @param extra_fields Comma separated list of extra fields to include
 #' @importFrom googleAuthR gar_api_generator
+#' @note For info about parameters see \url{https://developers.google.com/cloud-print/docs/appInterfaces#search}
 #' @export
 #'
 
@@ -10,10 +15,29 @@ gcp_search <- function(name = NULL,
                        connection_status = NULL,
                        use_cdd = NULL,
                        extra_fields = NULL) {
+  if (!is.null(type)) {
+    type <- match.arg(
+      type,
+      c(
+        "GOOGLE",
+        "HP",
+        "DOCS",
+        "DRIVE",
+        "FEDEX",
+        "ANDROID_CHROME_SNAPSHOT",
+        "IOS_CHROME_SNAPSHOT"
+      )
+    )
+  }
+  
+  if (!is.null(connection_status)) {
+    connection_status <- match.arg(connection_status,
+                                   c("ALL", "ONLINE", "UNKNOWN", "OFFLINE", "DORMANT"))
+  }
+  
   f <- googleAuthR::gar_api_generator(
     "https://www.google.com/cloudprint/search",
     data_parse_function = function(x) {
-      # print(x)
       x$printers
     },
     pars_args = list(
@@ -22,7 +46,8 @@ gcp_search <- function(name = NULL,
       connection_status = '',
       use_cdd = '',
       extra_fields = ''
-    )
+    ),
+    checkTrailingSlash = FALSE
   )
   
   f(
@@ -37,59 +62,107 @@ gcp_search <- function(name = NULL,
 }
 
 #' Get details about printer
-#' 
-#' @param printerid
+#'
+#' @param printerid The ID of the printer
+#' @param client See notes
+#' @inheritParams gcp_search
+#' @note Client field lets user augment printer capabilities see \url{https://developers.google.com/cloud-print/docs/appInterfaces#printer}
 #' @export
-#' 
+#'
 
-gcp_printer <- function(printerid, client = NULL, extra_fields = NULL) {
-  f <- googleAuthR::gar_api_generator(
-    "https://www.google.com/cloudprint/printer",
-    data_parse_function = function(x) {
-      # print(x)
-      x$printers
-    },
-    pars_args = list(
-      printerid = '',
-      client = '',
-      extra_fields = ''
+gcp_printer <-
+  function(printerid,
+           client = NULL,
+           extra_fields = NULL) {
+    f <- googleAuthR::gar_api_generator(
+      "https://www.google.com/cloudprint/printer",
+      data_parse_function = function(x) {
+        x$printers
+      },
+      pars_args = list(
+        printerid = '',
+        client = '',
+        extra_fields = ''
+      ),
+      checkTrailingSlash = FALSE
     )
+    
+    f(pars_arguments = list(
+      printerid = printerid,
+      client = client,
+      extra_fields = extra_fields
+    ))
+  }
+
+#' Accept printer invitation
+#'
+#' @inheritParams gcp_printer
+#' @param accept Boolean indicating whether to accept request from printer
+#' @export
+#' @note Undocumented API discovered at \url{https://stackoverflow.com/a/36366114/4564432}
+#'
+
+gcp_processinvite <- function(printerid, accept = TRUE) {
+  f <- googleAuthR::gar_api_generator(
+    "https://www.google.com/cloudprint/processinvite",
+    http_header = "POST",
+    data_parse_function = function(x) {
+      x$status
+    },
+    pars_args = list(printerid = '',
+                     accept = TRUE),
+    checkTrailingSlash = FALSE
   )
   
-  f(pars_arguments = list(
-    printerid = printerid,
-    client = client,
-    extra_fields = extra_fields
-  ))
+  f(pars_arguments = list(printerid = printerid,
+                          accept = accept))
+  
 }
 
 #' Submit print job to printer
+#'
+#' @importFrom jsonlite toJSON unbox
+#' @inheritParams gcp_printer
+#' @param title Title of print job
+#' @param ticket Print ticket, see notes
+#' @param content Document to print, see notes
+#' @param contentType MIME type of document to print
+#' @param tag Tags to add to print job
+#' @param http_header Either GET or POST, but only GET works at moment
+#' @note See \url{https://developers.google.com/cloud-print/docs/appInterfaces#submit}
+#'
 #' @export
-#' 
+#'
 
 gcp_submit <- function(printerid,
                        title,
                        ticket = NULL,
                        content,
                        contentType = NULL,
-                       tag = NULL) {
+                       tag = NULL,
+                       http_header = c("GET", "POST")) {
+  http_header <- match.arg(http_header)
   f <- googleAuthR::gar_api_generator(
     "https://www.google.com/cloudprint/submit",
-    # http_header = "POST",
+    http_header = http_header,
     data_parse_function = function(x) {
-      x$status
+      x$success
     },
     pars_args = list(
       printerid = '',
       title = '',
-      ticket = '',
-      content='',
-      contentType='',
-      tag=''
-    )
+      ticket = jsonlite::toJSON(list(
+        version = jsonlite::unbox("1.0"),
+        print = c()
+      ), auto_unbox = FALSE),
+      content = '',
+      contentType = '',
+      tag = ''
+    ),
+    checkTrailingSlash = FALSE
   )
   
-  f(the_body = content, 
+  f(
     pars_arguments = list(
       printerid = printerid,
       title = title,
